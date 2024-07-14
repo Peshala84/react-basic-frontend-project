974e85fb143f416ea7892510d109b8de222870dd
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-07-14 22:29:18 +0530

    update about.jsx and added about.css file

--- react-app/src/Componenet/Page/About.jsx
+++ react-app/src/Componenet/Page/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './About.css'
 
 export const About = () => {
   return (
-    <div>
-        ABOUT
+    <div className='about-part'>
+      <h1>About Us</h1>
+     <section>
+      <h2>History</h2>
+    
+      </section> 
+      <section>
+        <h2>Team</h2>
+
+      </section>
+      <section>
+        <h2>Values</h2>
+
+      </section>
     </div>
+    
   )
 }
